7d7795956b455b8cd375287ea8c612f8990afed9
HoisubLee <5chips17@gmail.com>
2022-08-20 14:08:59 +0900

    [hoisub]<build>기본 프로젝트 commit

---
+++ src/main/java/com/hanghae/wisely/WiselyApplication.java
@@ -0,0 +1,13 @@
+package com.hanghae.wisely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiselyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiselyApplication.class, args);
+	}
+
+}
