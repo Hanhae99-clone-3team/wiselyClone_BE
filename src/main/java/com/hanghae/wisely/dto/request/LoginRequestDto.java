b60c24364b7a1ba44aa2f4730cefc9dd534064b0
choonge07 <choonge_@naver.com>
2022-08-22 10:29:52 +0900

    [choonge07] <bug> @NoArgsConstructor 추가

--- src/main/java/com/hanghae/wisely/dto/request/LoginRequestDto.java
+++ src/main/java/com/hanghae/wisely/dto/request/LoginRequestDto.java
@@ -2,8 +2,10 @@ package com.hanghae.wisely.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class LoginRequestDto {
     private String email;
--- src/main/java/com/hanghae/wisely/dto/request/SignUpRequestDto.java
+++ src/main/java/com/hanghae/wisely/dto/request/SignUpRequestDto.java
@@ -2,9 +2,11 @@ package com.hanghae.wisely.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @Getter
+@NoArgsConstructor
 public class SignUpRequestDto {
 
     private String email;
