4797fdd025e9faa418239905eac6bf996a0daad6
tanibourne <tanibourne@gmail.com>
2022-08-23 13:56:52 +0900

    [hyuk] <feature> EC2 Redis connect 설정

--- src/main/java/com/hanghae/wisely/WiselyApplication.java
+++ src/main/java/com/hanghae/wisely/WiselyApplication.java
@@ -2,10 +2,12 @@ package com.hanghae.wisely;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableCaching
 public class WiselyApplication {
 
 	public static void main(String[] args) {
