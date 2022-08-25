a43f04a7a0e139635298bdb6a7706d341e4cae32
HoisubLee <5chips17@gmail.com>
2022-08-25 15:32:30 +0900

    [hoisub] <fix> 장바구니 추가 되지 않는 문제_재수정

--- src/main/java/com/hanghae/wisely/repository/CartRepository.java
+++ src/main/java/com/hanghae/wisely/repository/CartRepository.java
@@ -9,6 +9,8 @@ import org.springframework.data.jpa.repository.Query;
 public interface CartRepository extends JpaRepository<Cart, Long> {
     // 멤버 추가 필요
     // 결제 되지 앟은 장바구니 검색
-    @Query("SELECT c from Cart c where c.idPaid = false and c.member = :member ")
-    Cart findByIsPaidFalse(@Param("member")Member member);
+//    @Query(
+//            "SELECT c from Cart c where c.paid = false and c.member = :member ")
+//    Cart findByIsPaidFalse(@Param("member")Member member);
+    Cart findByPaidIsFalseAndMember(Member member);
 }
